a6e5811c6ceff36e28dd23d26986e886dd879cc2
Rheina <test@tests-Mac-mini.local>
2023-02-23 19:36:23 -0500

    added jwt

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,4 +1,7 @@
 const {User} = require('../models/index');
+const { decode } = require("../helpers/bcrypt");
+const { sign } = require("../helpers/jwt");
+
 class UserController {
   static async registerAdmin(req, res, next) {
     try {
@@ -20,6 +23,40 @@ class UserController {
     } catch (err) {
       res.send(500).json({ message: "Internal server error" });
         console.log(err)
+    }
+  }
+  static async loginAdmin(req,res,next){
+    try {
+      const { email, password } = req.body;
+      const foundUser = await User.findOne({
+        where: {
+          email,
+        },
+      });
+      if (!foundUser) {
+        res.send(501).json({message: "incorect email or password"})
+      } else {
+        const match = decode(password, foundUser.password);
+        if (!match) {
+          res.send(500).json({ message: "Internal server error" });
+          console.log(err)
+        } else {
+          const access_token = sign({
+            id: foundUser.id,
+            name: foundUser.name,
+            email: foundUser.email,
+            role: foundUser.role,
+          });
+          res.status(200).json({
+            access_token,
+            email: foundUser.email,
+            role: foundUser.role,
+            name: foundUser.name,
+          });
+        }
+      }
+    } catch (error) {
+      console.log(error)
     }
   }
     
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
 const UserController = require('../controllers/UserController');
 router.post("/register", UserController.registerAdmin)
+router.post("/login", UserController.loginAdmin)
 module.exports = router;
