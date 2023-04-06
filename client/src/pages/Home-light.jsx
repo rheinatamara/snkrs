d879ee5723c0277cd5346f935f4365c7c4eb513b
Rheina <test@tests-Mac-mini.local>
2023-04-06 12:39:37 -0400

    added few lines of code

--- client/src/pages/Home-light.jsx
+++ client/src/pages/Home-light.jsx
@@ -34,6 +34,56 @@ const Home = () => {
                             <h1 >NIKE</h1>
                         </div>
                 </div>
+                <div className="header-wrapper">
+                        <div className="bar"></div>
+                        <div className="description-box">
+                            <div className="wrapper">
+                                <h1 className='title plus-jakarta-font'>Nike Pegasus Trail 4 GORE-TEXT</h1>
+                                <p className='description font-m'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <div className="button-price">
+                                    <div className="button">
+                                        <p>Add to Bag <img src={rightArrow} alt="" /></p>
+                                    </div>
+                                    <div className="price plus-jakarta-font">
+                                        <p>$160</p>
+                                        
+                                    </div>
+                                </div>
+
+                            </div>
+                          
+                           
+                        </div>
+                        <div className="image-box">
+                            <img src={nike1} alt="" />
+                            <h1 >NIKE</h1>
+                        </div>
+                </div>
+                <div className="header-wrapper">
+                        <div className="bar"></div>
+                        <div className="description-box">
+                            <div className="wrapper">
+                                <h1 className='title plus-jakarta-font'>Nike Pegasus Trail 4 GORE-TEXT</h1>
+                                <p className='description font-m'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <div className="button-price">
+                                    <div className="button">
+                                        <p>Add to Bag <img src={rightArrow} alt="" /></p>
+                                    </div>
+                                    <div className="price plus-jakarta-font">
+                                        <p>$160</p>
+                                        
+                                    </div>
+                                </div>
+
+                            </div>
+                          
+                           
+                        </div>
+                        <div className="image-box">
+                            <img src={nike1} alt="" />
+                            <h1 >NIKE</h1>
+                        </div>
+                </div>
             </header>
         </div>
     );
