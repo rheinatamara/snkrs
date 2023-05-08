1400c5257dd71b30356b667c9695f9415697aa22
Rheina <test@tests-Mac-mini.local>
2023-05-08 16:04:55 -0400

    modificatin on homepage

--- client/src/pages/Home-light.jsx
+++ client/src/pages/Home-light.jsx
@@ -4,15 +4,17 @@ import nike1 from '../img/sneakers/nike1.png'
 import rightArrow from '../img/right-arrow.png'
 import '../scss/home.scss'
 const Home = () => {
+ 
     return (
+        
         <div className="homepage">
             <div className="gradient1"></div>
             <div className="gradient2"></div>
-            {/* <div className="gradient3"></div> */}
+            <div className="gradient3"></div>
             <Navbar></Navbar>
-            
-            {/* <div className="gradient"></div>
-            <div className="gradient"></div> */}
+               
+            <div className="gradient"></div>
+            <div className="gradient"></div>
               <div className="numbers">
                                 <ul>
                                     <li className='one'>01</li>
@@ -104,32 +106,9 @@ const Home = () => {
                 
               
             </header>
-            {/* <div className="header-wrapper">
-                        <div className="bar"></div>
-                        <div className="description-box">
-                            <div className="wrapper">
-                                <h1 className='title plus-jakarta-font'>Nike Pegasus Trail 4 GORE-TEXT</h1>
-                                <p className='description font-m'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                <div className="button-price">
-                                    <div className="button">
-                                        <p>Add to Bag <img src={rightArrow} alt="" /></p>
-                                    </div>
-                                    <div className="price plus-jakarta-font">
-                                        <p>$160</p>
-                                        
-                                    </div>
-                                </div>
+           
+     </div>
 
-                            </div>
-                          
-                           
-                        </div>
-                        <div className="image-box">
-                            <img src={nike1} alt="" />
-                            <h1 >NIKE</h1>
-                        </div>
-                </div> */}
-        </div>
         
     );
 }
