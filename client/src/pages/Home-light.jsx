f938d7ef72c70436b9bc02fafaed83b3f948d17f
Rheina <test@tests-Mac-mini.local>
2023-05-18 15:12:21 -0400

    client upgrades

--- client/src/pages/Home-light.jsx
+++ client/src/pages/Home-light.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import Navbar from '../components/Navbar-light'
 import nike1 from '../img/sneakers/nike1.png'
 import rightArrow from '../img/right-arrow.png'
+import sneakers1 from '../img/sneakers/sneakers1.png'
+// import Bag from "../img/Bag.png";
+// import backgroundSVG from '../img/svg/backgroundSVG.svg'
+
 import '../scss/home.scss'
 const Home = () => {
  
     return (
         
         <div className="homepage">
-            <div className="gradient1"></div>
+            {/* <div className="gradient1"></div>
             <div className="gradient2"></div>
-            <div className="gradient3"></div>
+            <div className="gradient3"></div> */}
             <Navbar></Navbar>
                
             <div className="gradient"></div>
@@ -26,7 +30,7 @@ const Home = () => {
             <header>
                 <div className="header">
                   
-                    <div className="header-wrapper">
+                    <div className="header-wrapper ">
                         
                         <div className="description-box">
                             <div className="wrapper">
@@ -51,7 +55,7 @@ const Home = () => {
                             <h1 >NIKE</h1>
                         </div>
                 </div>
-                <div className="header-wrapper">
+                <div className="header-wrapper ">
                         <div className="bar"></div>
                         <div className="description-box">
                             <div className="wrapper">
@@ -76,7 +80,7 @@ const Home = () => {
                             <h1 >NIKE</h1>
                         </div>
                 </div>
-                <div className="header-wrapper">
+                <div className="header-wrapper ">
                         <div className="bar"></div>
                         <div className="description-box">
                             <div className="wrapper">
@@ -106,6 +110,111 @@ const Home = () => {
                 
               
             </header>
+            <section id='popular-picks'>
+                <h1 className="plus-jakarta-font semiBold">Popular <span className='purple'>Picks</span></h1>
+                <div className="product-container">
+                <div className="product-grid">
+                    <div className="product-outer">
+                        <div className="product-inner">
+                            <div className="upper-section">
+                                <div className="color"></div>
+                                <div className="bag"></div>
+
+                            </div>
+                            <div className="product">
+                                <img src={nike1} alt="" />
+                                <p>Pegasus Trail 4 GORE-TEX</p>
+                            </div>
+
+                        </div>
+                        <div className="bottom-text">
+                            <p className="price">$130</p>
+                            <p className="detail">See Detail </p>
+                        </div>
+                    </div>
+                    <div className="product-outer">
+                        <div className="product-inner">
+                            <div className="upper-section">
+                                <div className="color"></div>
+                                <div className="bag"></div>
+
+                            </div>
+                            <div className="product">
+                                <img src={nike1} alt="" />
+                                <p>Pegasus Trail 4 GORE-TEX</p>
+                            </div>
+
+                        </div>
+                        <div className="bottom-text">
+                            <p className="price">$130</p>
+                            <p className="detail">See Detail </p>
+                        </div>
+                    
+                    </div>
+                    <div className="product-outer">
+                        <div className="product-inner">
+                            <div className="upper-section">
+                                <div className="color"></div>
+                                <div className="bag"></div>
+
+                            </div>
+                            <div className="product">
+                                <img src={nike1} alt="" />
+                                <p>Pegasus Trail 4 GORE-TEX</p>
+                            </div>
+
+                        </div>
+                        <div className="bottom-text">
+                            <p className="price">$130</p>
+                            <p className="detail">See Detail </p>
+                        </div>
+                    
+                    </div>
+                    <div className="product-outer">
+                        <div className="product-inner">
+                            <div className="upper-section">
+                                <div className="color"></div>
+                                <div className="bag"></div>
+
+                            </div>
+                            <div className="product">
+                                <img src={nike1} alt="" />
+                                <p>Pegasus Trail 4 GORE-TEX</p>
+                            </div>
+
+                        </div>
+                        <div className="bottom-text">
+                            <p className="price">$130</p>
+                            <p className="detail">See Detail </p>
+                        </div>
+                    
+                    </div>
+                </div>
+                    
+                </div>
+                
+            </section>
+            <section id="content">
+                <div className="content-1">
+                    <div className="product">
+                        
+                        <div className="product-content">
+                            <h3 className="plus-jakarta-font font-m">Get up to 30% off</h3>
+                            <p>Lorem ipsum dolor sit amet.</p>
+                        </div>
+                        <div className="image">
+                            <img src={sneakers1} alt="" />
+                        </div>
+                    </div>
+                   
+                    <div className="text">
+                        <h1>We Provide Authentic Shoes.</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam magnam consequuntur quaerat sunt qui nulla, quo nobis eius amet harum molestias officia modi sed excepturi.</p>
+                    </div>
+                   
+                </div>
+               
+            </section>
            
      </div>
 
